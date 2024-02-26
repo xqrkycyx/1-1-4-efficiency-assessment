6a400f3a465111687dfb2a72068cd5a8498aaa00
Will Williams <wwilliams@indeed.com>
2024-02-25 18:58:43 -0800

    Adds 'cable 2'

--- src/cable.js
+++ src/cable.js
@@ -45,4 +45,33 @@ function cable1(currentLength, requiredLength, saleLength) {
  Implement algorithm 2 here
 */
 
-module.exports = { cable1 };
+// PSEUDOCODE:
+// function cable2
+//    // accepts: currentLength - an integer between 1 and 1,000,000,000
+//    //          requireLength - an integer between 1 and 1,000,000,000
+//    //          saleLength - an integer between 1 and 1,000,000,000
+
+//    declare a variable named distance, assign it to the difference between requiredLength and currentlength
+//    declare a variable named total,
+//    divide distance by saleLength and round up to the nearest integer, assign this value to total
+//    return total
+
+/**
+ * Calculate the minimum number of lengths of cable that need to be purchased
+ * @param {number} currentLength - The length of cable that is already possessed by the company
+ * @param {number} requiredLength - The total length of cable needed for the task
+ * @param {number} saleLength - The length of a single piece of cable that can be bought
+ * @returns {number} - The minimum number of lengths of cable needed to be purchased
+ */
+function cable2(currentLength, requiredLength, saleLength) {
+  // Declare a variable named distance, assign it to the difference between requiredLength and currentlength
+  const distance = requiredLength - currentLength;
+
+  // Declare a variable named total, divide distance by saleLength and round up to the nearest integer, assign this value to total
+  const total = Math.ceil(distance / saleLength);
+
+  // Return total
+  return total;
+}
+
+module.exports = { cable1, cable2 };
